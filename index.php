68d6ba37b368929d0664439c81a505408d66048c
applepine07 <mrchildrenmissadult@hotmail.com>
2022-04-03 11:23:14 +0800

    九九乘法ok

--- index.php
+++ index.php
@@ -1,19 +1,37 @@
 <style>
-    table{
-        border-collapse:collapse;
+    table {
+        border-collapse: collapse;
+        font-family: 'Microsoft JhengHei';
+        text-align: center;
     }
-    td{
-        border: 1px solid lightcoral;
+
+    td {
+        border: 1px solid slategray;
+        width: 30px;
+        height: 30px;
     }
 </style>
 <table>
-<?php
-for($i=1;$i<=9;$i++){
-    echo "<tr>";
-    for($j=1;$j<=9;$j++){
-        echo "<td>".$i*$j."</td>";
+    <?php
+    for ($i = 0; $i <= 9; $i++) {
+        // 9列
+        echo "<tr>";
+        for ($j = 0; $j <= 9; $j++) {
+            if ($i == 0 && $j == 0) {
+                // 最左上角
+                echo "<td style='background-color:slategray; color:white'></td>";
+            } else if ($i == 0) {
+                // 第一列
+                echo "<td style='background-color:slategray; color:white'>$j</td>";
+            } else if ($j == 0) {
+                // 第一欄
+                echo "<td style='background-color:slategray; color:white'>$i</td>";
+            } else {
+                // 以上三種情形外的其它情形
+                echo "<td>" . $i * $j . "</td>";
+            }
+        }
+        echo "</tr>";
     }
-    echo "</tr>";
-}
-?>
+    ?>
 </table>
